fb2a3f6161690ff1ba14dcee897c6bd63d1187ff
theginox <gpuri.work@gmail.com>
2024-06-05 10:59:48 +0200

    Create gcp-org.tf
    Adding GCP Org example

---
+++ sysdig-cloud-integrations/examples-terraform-scripts/gcp-org.tf
@@ -0,0 +1,134 @@
+provider "google" {
+  project = "[Target GCP Project ID]"
+  region  = "us-central1"
+}
+
+module "organization-posture" {
+  source               = "sysdiglabs/secure/google//modules/services/service-principal"
+  project_id           = "[Target GCP Project ID]"
+  service_account_name = "sysdig-secure-q6re"
+  is_organizational    = true
+  organization_domain  = "organizatin.domain"
+}
+
+module "organization-threat-detection" {
+  source            	= "sysdiglabs/secure/google//modules/services/webhook-datasource"
+  project_id        	= "[Target GCP Project ID]"
+  push_endpoint     	= "[Sysdig Endpoint]"
+  external_id           = "[Sysdig External ID]"
+  is_organizational 	= true
+  organization_domain = "organizatin.domain"
+}
+
+module "vm-host-scan" {
+	source				= "sysdiglabs/secure/google//modules/services/agentless-scan"
+	project_id			= "[Target GCP Project ID]"
+	worker_identity		= "agentless-worker-sa@prod-sysdig-agentless.iam.gserviceaccount.com"
+	sysdig_backend		= "[Sysdig Backend ID]"
+	is_organizational	= true
+	organization_domain	= "organizatin.domain"
+}
+terraform {
+
+  required_providers {
+    sysdig = {
+      source  = "sysdiglabs/sysdig"
+      version = "~> 1.24.2"
+    }
+  }
+}
+
+provider "sysdig" {
+  sysdig_secure_url       = "[Sysdig Endpoint]"
+  sysdig_secure_api_token = "[Sysdig API Token]"
+}
+
+resource "sysdig_secure_cloud_auth_account" "gcp_project_[Target GCP Project ID]" {
+  enabled       = true
+  provider_id   = "[Target GCP Project ID]"
+  provider_type = "PROVIDER_GCP"
+
+  feature {
+
+    secure_threat_detection {
+      enabled    = true
+      components = ["COMPONENT_WEBHOOK_DATASOURCE/secure-runtime"]
+    }
+
+    secure_identity_entitlement {
+      enabled    = true
+      components = ["COMPONENT_WEBHOOK_DATASOURCE/secure-runtime"]
+    }
+
+    secure_config_posture {
+      enabled    = true
+      components = ["COMPONENT_SERVICE_PRINCIPAL/secure-posture"]
+    }
+
+    secure_agentless_scanning {
+      enabled    = true
+      components = ["COMPONENT_SERVICE_PRINCIPAL/secure-scanning"]
+    }
+  }
+  component {
+    type     = "COMPONENT_SERVICE_PRINCIPAL"
+    instance = "secure-posture"
+    service_principal_metadata = jsonencode({
+      gcp = {
+        key = module.organization-posture.service_account_key
+      }
+    })
+  }
+  component {
+    type     = "COMPONENT_WEBHOOK_DATASOURCE"
+    instance = "secure-runtime"
+    webhook_datasource_metadata = jsonencode({
+      gcp = {
+        webhook_datasource = {
+          pubsub_topic_name      = module.organization-threat-detection.ingestion_pubsub_topic_name
+          sink_name              = module.organization-threat-detection.ingestion_sink_name
+          push_subscription_name = module.organization-threat-detection.ingestion_push_subscription_name
+          push_endpoint          = module.organization-threat-detection.push_endpoint
+          routing_key            = "[Sysdig Routing Key]"
+        }
+        service_principal = {
+          workload_identity_federation = {
+            pool_id          = module.organization-threat-detection.workload_identity_pool_id
+            pool_provider_id = module.organization-threat-detection.workload_identity_pool_provider_id
+            project_number   = module.organization-threat-detection.workload_identity_project_number
+          }
+          email = module.organization-threat-detection.service_account_email
+        }
+      }
+    })
+  }
+  component {
+    type     = "COMPONENT_SERVICE_PRINCIPAL"
+    instance = "secure-scanning"
+    service_principal_metadata = jsonencode({
+      gcp = {
+        workload_identity_federation = {
+          pool_provider_id = module.vm-host-scan.workload_identity_pool_provider
+        }
+        email = module.vm-host-scan.controller_service_account
+      }
+    })
+  }
+  component {
+    type     = "COMPONENT_SERVICE_PRINCIPAL"
+    instance = "secure-onboarding"
+    service_principal_metadata = jsonencode({
+      gcp = {
+        key = module.organization-posture.service_account_key
+      }
+    })
+  }
+  depends_on = [module.organization-posture, module.organization-threat-detection, module.vm-host-scan]
+}
+
+resource "sysdig_secure_organization" "gcp_organization_[Target GCP Project ID]" {
+  organizational_unit_ids = []
+  management_account_id   = sysdig_secure_cloud_auth_account.gcp_project_[Target GCP Project ID].id
+  depends_on              = [module.organization-posture, module.organization-threat-detection, module.vm-host-scan]
+}
+
