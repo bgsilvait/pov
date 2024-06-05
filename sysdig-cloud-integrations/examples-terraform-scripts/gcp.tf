2e213e0f38f8dab3c5bd95aea90597745c5071dc
theginox <gpuri.work@gmail.com>
2024-06-05 14:58:39 +0200

    Updated script according to the 1.24.2 v

--- sysdig-cloud-integrations/examples-terraform-scripts/gcp.tf
+++ sysdig-cloud-integrations/examples-terraform-scripts/gcp.tf
@@ -1,12 +1,12 @@
 provider "google" {
   project = "[Target GCP Project ID]"
-  region  = "us-central1"
+  region  = "[Target GCP Region]"
 }
 
 module "project-posture" {
   source               = "sysdiglabs/secure/google//modules/services/service-principal"
   project_id           = "[Target GCP Project ID]"
-  service_account_name = "sysdig-secure-2ljs"
+  service_account_name = "[Service Account Name]"
 }
 
 module "single-project-threat-detection" {
@@ -16,12 +16,18 @@ module "single-project-threat-detection" {
   external_id   = "[Sysdig External ID]"
 }
 
+module "vm-host-scan" {
+	source			= "sysdiglabs/secure/google//modules/services/agentless-scan"
+	project_id		= "[Target GCP Project ID]"
+	worker_identity	= "agentless-worker-sa@prod-sysdig-agentless.iam.gserviceaccount.com"
+	sysdig_backend	= "[Sysdig Backend ID]"
+}
 terraform {
 
   required_providers {
     sysdig = {
       source  = "sysdiglabs/sysdig"
-      version = "~> 1.23.2"
+      version = "~> 1.24.2"
     }
   }
 }
@@ -40,17 +46,22 @@ resource "sysdig_secure_cloud_auth_account" "gcp_project_[Target GCP Project ID]
 
     secure_threat_detection {
       enabled    = true
-      components = ["COMPONENT_WEBHOOK_DATASOURCE/secure-runtime", "COMPONENT_SERVICE_PRINCIPAL/secure-runtime"]
+      components = ["COMPONENT_WEBHOOK_DATASOURCE/secure-runtime"]
     }
 
     secure_identity_entitlement {
       enabled    = true
-      components = ["COMPONENT_SERVICE_PRINCIPAL/secure-posture"]
+      components = ["COMPONENT_WEBHOOK_DATASOURCE/secure-runtime"]
     }
 
     secure_config_posture {
       enabled    = true
       components = ["COMPONENT_SERVICE_PRINCIPAL/secure-posture"]
+    }
+
+    secure_agentless_scanning {
+      enabled    = true
+      components = ["COMPONENT_SERVICE_PRINCIPAL/secure-scanning"]
     }
   }
   component {
@@ -72,24 +83,31 @@ resource "sysdig_secure_cloud_auth_account" "gcp_project_[Target GCP Project ID]
           sink_name              = module.single-project-threat-detection.ingestion_sink_name
           push_subscription_name = module.single-project-threat-detection.ingestion_push_subscription_name
           push_endpoint          = module.single-project-threat-detection.push_endpoint
+          routing_key            = "[Sysdig Routing Key]"
+        }
+        service_principal = {
+          workload_identity_federation = {
+            pool_id          = module.single-project-threat-detection.workload_identity_pool_id
+            pool_provider_id = module.single-project-threat-detection.workload_identity_pool_provider_id
+            project_number   = module.single-project-threat-detection.workload_identity_project_number
+          }
+          email = module.single-project-threat-detection.service_account_email
         }
       }
     })
   }
   component {
     type     = "COMPONENT_SERVICE_PRINCIPAL"
-    instance = "secure-runtime"
+    instance = "secure-scanning"
     service_principal_metadata = jsonencode({
       gcp = {
         workload_identity_federation = {
-          pool_id          = module.single-project-threat-detection.workload_identity_pool_id
-          pool_provider_id = module.single-project-threat-detection.workload_identity_pool_provider_id
-          project_number   = module.single-project-threat-detection.workload_identity_project_number
+          pool_provider_id = module.vm-host-scan.workload_identity_pool_provider
         }
-        email = module.single-project-threat-detection.service_account_email
+        email = module.vm-host-scan.controller_service_account
       }
     })
   }
-  depends_on = [module.project-posture, module.single-project-threat-detection]
+  depends_on = [module.project-posture, module.single-project-threat-detection, module.vm-host-scan]
 }
 
